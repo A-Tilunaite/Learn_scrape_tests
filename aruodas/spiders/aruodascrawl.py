90f759c0974a1829c3e14399b6067b35506935ee
A-Tilunaite <44075640+A-Tilunaite@users.noreply.github.com>
2021-06-27 22:32:31 +1000

    Update aruodascrawl.py

--- aruodas/spiders/aruodascrawl.py
+++ aruodas/spiders/aruodascrawl.py
@@ -6,16 +6,16 @@ class aruodascrawl(scrapy.Spider):
 
     def parse(self,response):
         for flats in response.css('a.result-item-info-container-v3'):
-            dummy=flats.css('span.item-address-v3::text').get().strip().split(',')
+            region_street=flats.css('span.item-address-v3::text').get().strip().split(',')
             all_info=flats.css('span.item-description-v3::text').get()
             s1 = all_info.find('rooms')
             s2 = all_info.find('m²')
             s3 = all_info.find('floor')
             s4 = all_info.find('year')
-            yield{
+            yield {
                 'Pavadinimas': flats.css('span.item-address-v3::text').get().strip(),
-                'Rajonas': dummy[0],
-                'Gatve' : dummy[1],
+                'Rajonas':  region_street[0],
+                'Gatve' :  region_street[1],
                 'Kaina': flats.css('span.item-price-main-v3').xpath('span/text()').get().replace('€',''),
                 'Visa_info': all_info,
                 'Dydis': all_info[(s1+6):(s2)].replace(',','.'), 
@@ -26,6 +26,5 @@ class aruodascrawl(scrapy.Spider):
             }
         next_page = response.css('div.button-next-v2').css('a').attrib['href']
         first_url=response.request.url
-        if next_page is not None:
-            if first_url.find(next_page)==-1:
+        if next_page is not None and first_url.find(next_page)==-1:
                 yield response.follow(next_page ,callback=self.parse)
